1c09c1b4e0ce326b053612dbe33767265f402ac2
takatoshiH <t.hikijiri@gmail.com>
2020-02-09 22:00:37 +0900

    ObservedWeatherモデルにバリデーションを記述した

--- ForecastGetter/app/models/observed_weather.rb
+++ ForecastGetter/app/models/observed_weather.rb
@@ -1,2 +1,6 @@
 class ObservedWeather < ApplicationRecord
+  validates :highest_temperature, presence: true, numericality: true
+  validates :lowest_temperature, presence: true, numericality: true
+  validates :weather, presence: true
+  validates :date, presence: true
 end
