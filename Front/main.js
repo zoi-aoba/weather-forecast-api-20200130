3aa44ff2f40302c3a8da934bb040b344b20d1c96
takatoshiH <t.hikijiri@gmail.com>
2020-02-12 01:51:33 +0900

    認証関連の修正など

--- Front/main.js
+++ Front/main.js
@@ -8,15 +8,17 @@ var firebaseConfig = {
   appId: "1:605379077010:web:9f67306fd37f96d4bdd74b"
 };
 
-function initApp() {
-  firebase.initializeApp(firebaseConfig);
 
+firebase.initializeApp(firebaseConfig);
+  
+function initApp() {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       var email = user.email;
       var uid = user.uid;
 
       console.log(email);
+      document.getElementById("email").innerText = email;
     } else {
       console.log("ログインできていません");
       window.location.href = 'sign_up.html';
@@ -24,6 +26,13 @@ function initApp() {
   });
 }
 
-window.onload = function() {
+window.onload = () => {
   initApp();
 };
+
+// ログアウト機能の作成
+window.logout = () => {
+  firebase.auth().signOut();
+  window.location.href = 'sign_in.html';
+}
+
