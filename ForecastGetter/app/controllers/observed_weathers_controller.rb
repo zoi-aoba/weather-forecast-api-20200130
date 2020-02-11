13d8315d3901e5ca913eda05e457dce1f222acec
takatoshiH <t.hikijiri@gmail.com>
2020-02-11 23:32:57 +0900

    細かい修正

--- ForecastGetter/app/controllers/observed_weathers_controller.rb
+++ ForecastGetter/app/controllers/observed_weathers_controller.rb
@@ -2,10 +2,10 @@ class ObservedWeathersController < ApplicationController
   require "date"
 
   def observed_weather
-    @observed_weathers = []
+    @observed_weathers = Hash.new
     (1..30).each do |number|
-      date = Date.today
-      @observed_weathers.push(ObservedWeather.find_by(date: date)) if ObservedWeather.exists?(date: date)
+      date = Date.today - number
+      @observed_weathers[date] = ObservedWeather.find_by(date: date) if ObservedWeather.exists?(date: date)
     end
     render json: { status: 'SUCCESS', data: @observed_weathers }
   end
