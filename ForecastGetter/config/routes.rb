20102383c30fa2b8b5744b27dc0d6da8fe902139
takatoshiH <t.hikijiri@gmail.com>
2020-02-16 14:35:52 +0900

    routes.rbの修正

--- ForecastGetter/config/routes.rb
+++ ForecastGetter/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace: api do
+  namespace :api do
     get "tommorow_forecast", to: "forecasts#tomorrow_forecast"
     get "get_observed_weather", to: "observed_weathers#observed_weather"
   end
