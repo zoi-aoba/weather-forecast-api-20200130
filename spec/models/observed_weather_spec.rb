fa8eb18d08cc1426ea76fab3867e2ff2311fbc54
takatoshiH <t.hikijiri@gmail.com>
2020-02-26 02:16:17 +0900

    テストの記述

--- spec/models/observed_weather_spec.rb
+++ spec/models/observed_weather_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe ObservedWeather, type: :model do
-  
+  describe 'convert_celsius' do
+    it '摂氏と華氏の変換が正しいか' do
+      expect(Forecast.convert_celsius(100)).to eq(37.8)
+    end
+  end
+
+  describe 'format_data' do
+    it 'Responseが適正でないときに例外が出るか' do
+    end
+
+    it 'Responseが適正なときには例外は出ないか' do
+    end
+
+    it 'Responseから必要なデータを取得できているか' do
+    end
+  end
 end
--- spec/models/requester_spec.rb
+++ spec/models/requester_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Requester, type: :model do
-  
+  describe 'get_data' do
+    it '正しくHTTPリクエストができているか' do
+    end
+  end
+
+  describe 'make_forecast_url' do
+    it '正しくURLが作成できているか' do
+    end
+  end
+
+  describe 'make_observed_weather_url' do
+    it '正しくURLが作成できているか' do
+    end
+  end
+
+  describe 'check_response' do
+    it 'Responseが200以外の時例外を出せるか' do
+    end
+
+    it 'Responseが200の時例外を出さないか' do
+    end
+  end
 end
